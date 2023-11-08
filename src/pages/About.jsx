e94a8f0f580b242a47e7cf61c921a42ae05401ac
Leroyg-11 <119599533+Leroyg-11@users.noreply.github.com>
2023-11-08 13:30:17 +0100

    first-commit

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -3,30 +3,50 @@ import "./Pages.scss";
 import AboutCard from "../components/projet/ProjetCard";
 import avatar from "../assets/Avatar.jpg";
 const About = () => {
-  const [appear, setAppear] = useState(false);
-
-  // Utilisez useEffect pour ajouter la classe "show" après le rendu initial
-  useEffect(() => {
-    setAppear(true);
-  }, []);
-
   return (
-    <section className={`fade-in ${appear ? "show" : ""}`}>
-      <h1>ABOUT</h1>
+    <main>
+      <h1>PROJETS</h1>
 
-      <p>
-        About me : <br />
-        <br />
-        About About About About About About About About About About About About
-        About About About About About About About About About About About
-      </p>
-      <p>
-        About me : <br />
-        <br />
-        About About About About About About About About About About About About
-        About About About About About About About About About About About
-      </p>
-    </section>
+      <section>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam fuga
+          blanditiis, asperiores repudiandae ab minima minus sapiente inventore
+          nemo ratione, maxime repellendus voluptates accusamus sequi libero
+          earum nostrum porro, quia molestiae ducimus fugit! Totam
+          reprehenderit, dignissimos porro impedit deserunt ducimus quidem
+          aliquid quos veritatis laudantium. Quam dolore quis temporibus
+          perspiciatis odio iusto saepe, error mollitia ipsam eaque facere magni
+          illo laudantium ullam quaerat amet ab sed. Maxime reiciendis quia
+          dicta voluptatibus labore veniam eligendi, pariatur obcaecati
+          molestias ipsum impedit quidem, necessitatibus provident, placeat iste
+          corrupti. Nostrum saepe aliquid voluptates ullam impedit minima
+          perferendis enim autem minus iste ipsam quidem laudantium quia
+          reiciendis aliquam similique tempora velit, atque obcaecati
+          repellendus aut excepturi modi. Dolore adipisci blanditiis quo amet.
+          Odit modi similique deleniti eius vero dolorum fugit, hic ratione
+          velit vitae quae delectus sint iusto corporis tempore. Error
+          doloremque dolorem ipsa totam, repudiandae fugiat esse perspiciatis
+          quis aspernatur id animi? Culpa reiciendis, excepturi fugit quia
+          debitis optio consequatur? Non, asperiores nobis. Adipisci nam quam
+          labore neque culpa doloremque alias, molestias iure quasi recusandae
+          illo cum quos. Dolores fuga vel rem quis. Est nostrum harum nulla
+          voluptatum quasi fuga veritatis consequuntur minus suscipit odit enim
+          dignissimos inventore impedit, quis quam perspiciatis quibusdam illum
+          nihil magnam! Aut incidunt aliquid doloremque necessitatibus similique
+          quos aspernatur nam repudiandae eligendi, eum ipsa laboriosam
+          voluptate? Laboriosam sit at ex vel necessitatibus beatae alias
+          doloremque asperiores voluptas molestias, iure magni, ipsum veritatis
+          cumque sed doloribus ducimus molestiae et, ipsam rerum? Aperiam,
+          pariatur repudiandae doloribus atque voluptatem delectus aliquam optio
+          sint aliquid iure fugit eveniet laborum soluta doloremque odit rem
+          enim maiores voluptate cumque ab! Dolores dolor velit excepturi
+          repellendus assumenda ipsa ex vitae maxime rerum soluta quis
+          architecto, minima asperiores. Tempore libero saepe quo adipisci,
+          voluptatem, temporibus earum eius sunt ipsa beatae perspiciatis
+          laudantium maxime corporis asperiores dignissimos nam?
+        </p>
+      </section>
+    </main>
   );
 };
 
--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,90 +1,96 @@
-import React, { useEffect, useState } from "react";
-import "./Pages.scss";
-const Contact = () => {
-  const [formData, setFormData] = useState({
-    nom: "",
-    prenom: "",
-    telephone: "",
-    email: "",
-    message: "",
-  });
+import React, { useRef, useState } from "react";
+import emailjs from "@emailjs/browser";
 
-  const handleInputChange = (e) => {
-    const { name, value } = e.target;
-    setFormData({
-      ...formData,
-      [name]: value,
-    });
+export const Contact = () => {
+  const [successMessage, setSuccessMessage] = useState("");
+
+  const handleSuccessMessageClose = () => {
+    setSuccessMessage("");
   };
 
-  const handleSubmit = (e) => {
+  const form = useRef();
+
+  const [nom, setNom] = useState("");
+  const [email, setEmail] = useState("");
+  const [telephone, setTelephone] = useState("");
+  const [message, setMessage] = useState("");
+
+  const sendEmail = (e) => {
     e.preventDefault();
-    // Ici, vous pouvez ajouter la logique pour soumettre les données du formulaire.
-    console.log(formData);
+
+    emailjs
+      .sendForm(
+        "service_r012cd6",
+        "template_j4ow6n3",
+        form.current,
+        "Lon5Jzk7nxbfjrV8O"
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+    setSuccessMessage("Votre message a été envoyé avec succès.");
+    setNom("");
+    setEmail("");
+    setTelephone("");
+    setMessage("");
   };
 
   return (
     <main>
+      <h1>CONTACTEZ MOI</h1>
       <section>
-        <form
-          action="https://formsubmit.co/guillaumeleroy21@gmail.com"
-          method="POST"
-        >
-          <label htmlFor="nom">Nom :</label>
-          <input
-            type="text"
-            id="nom"
-            name="nom"
-            required
-            value={formData.nom}
-            onChange={handleInputChange}
-          />
-
-          <label htmlFor="prenom">Prénom :</label>
-          <input
-            type="text"
-            id="prenom"
-            name="prenom"
-            required
-            value={formData.prenom}
-            onChange={handleInputChange}
-          />
-
-          <label htmlFor="telephone">Téléphone :</label>
-          <input
-            type="tel"
-            id="telephone"
-            name="telephone"
-            required
-            value={formData.telephone}
-            onChange={handleInputChange}
-          />
-
-          <label htmlFor="email">Email :</label>
-          <input
-            type="email"
-            id="email"
-            name="email"
-            required
-            value={formData.email}
-            onChange={handleInputChange}
-          />
-
-          <label htmlFor="message">Message :</label>
-          <textarea
-            id="message"
-            name="message"
-            rows="4"
-            cols="50"
-            required
-            value={formData.message}
-            onChange={handleInputChange}
-          />
-
-          <button type="submit" onSubmit={handleSubmit}>
-            Envoyer
-          </button>
-        </form>
+        <div className="form-container">
+          <form ref={form} onSubmit={sendEmail}>
+            <label>Nom</label>
+            <input
+              type="text"
+              name="user_name"
+              value={nom}
+              onChange={(e) => setNom(e.target.value)}
+            />
+            <label>Email</label>
+            <input
+              type="email"
+              name="user_email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            <label>Telephone</label>
+            <input
+              type="number"
+              name="user_phone"
+              value={telephone}
+              onChange={(e) => setTelephone(e.target.value)}
+            />
+            <label>Message</label>
+            <textarea
+              name="message"
+              value={message}
+              onChange={(e) => setMessage(e.target.value)}
+            />
+            <input type="submit" value="Send" />
+          </form>
+          <div
+            className={`message-container ${successMessage ? "" : "hidden"}`}
+          >
+            {successMessage && (
+              <>
+                <p
+                  className="close-message"
+                  onClick={handleSuccessMessageClose}
+                >
+                  x
+                </p>
+                <p className="success-message">{successMessage}</p>
+              </>
+            )}
+          </div>
+        </div>
       </section>
     </main>
   );
