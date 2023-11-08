5190305ede25cd2b06aab71edf5243139cc46195
Leroyg-11 <119599533+Leroyg-11@users.noreply.github.com>
2023-11-08 13:51:58 +0100

    failed test

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "https://leroyg-11.github.io/PortfolioGL",
+  base: "https://leroyg-11.github.io/",
   plugins: [react()],
 });
